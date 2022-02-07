52423da7810ca18f915e0a309d1a26a1d18af9bc
Rosmawati <90950041+Rosma640@users.noreply.github.com>
2022-02-07 18:40:38 +0700

    Create  Shape .java

---
+++ Shape .java
@@ -0,0 +1,10 @@
+public abstract class Shape {
+    String color;
+    void setColor(String color) {
+        this.color=color;
+    }
+    String getColor(){
+    return color;
+    }
+   abstract float getArea(); 
+}
